d3af67ce0b133d148da37965956a587bdcb06a23
Mischa Salle <msalle@nikhef.nl>
2019-04-17 11:45:47 +0200

    Cleanup exception handler
    no real need for our own exceptionhandler. We now only have a constructor
    calling the super's. Add comment in case we need something else.

--- vo-portal/src/main/java/eu/rcauth/voportal/client/oauth2/VPOA2ClientExceptionHandler.java
+++ vo-portal/src/main/java/eu/rcauth/voportal/client/oauth2/VPOA2ClientExceptionHandler.java
@@ -1,11 +1,7 @@
 package eu.rcauth.voportal.client.oauth2;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import javax.servlet.ServletException;
 
 import edu.uiuc.ncsa.myproxy.oa4mp.client.servlet.ClientServlet;
 import edu.uiuc.ncsa.oa4mp.oauth2.client.servlet.OA2ClientExceptionHandler;
@@ -13,27 +9,21 @@ import edu.uiuc.ncsa.security.servlet.ServiceClientHTTPException;
 import edu.uiuc.ncsa.security.core.util.MyLoggingFacade;
 
 /**
- * Exception handler for the VO portal servlets
+ * Exception handler for the VO portal servlets.
+ * The handling is done by
+ * {@link OA2ClientExceptionHandler#handleException(Throwable, HttpServletRequest, HttpServletResponse)}.
+ * Exceptions include a {@link ServiceClientHTTPException} which is
+ * thrown when an error is returned from the mp-server endpoint. In that
+ * case the content of the response is typically a HTML page which is parsed
+ * by {@link OA2ClientExceptionHandler#parseContent(String, HttpServletRequest)}.
  */
 public class VPOA2ClientExceptionHandler extends OA2ClientExceptionHandler {
 
-    MyLoggingFacade logger;
+//  MyLoggingFacade logger;
 
     public VPOA2ClientExceptionHandler(ClientServlet clientServlet, MyLoggingFacade myLogger) {
         super(clientServlet, myLogger);
-        this.logger = myLogger;
-    }
-
-    /**
-     * handles exceptions, including a {@link ServiceClientHTTPException} which is
-     * thrown when an error is returned from the mp-server endpoint. In that
-     * case the content of the response is typically a HTML page which is parsed
-     * by {@link OA2ClientExceptionHandler#parseContent(String, HttpServletRequest)}
-     */
-    @Override
-    public void handleException(Throwable t, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        logger.warn("Handling exception: " + t.getMessage());
-        super.handleException(t, request, response);
+//      this.logger = myLogger;
     }
 
 }
