0325fcf9d2da43ff36e89e502b5aef7cb7e27611
Mischa Salle <msalle@nikhef.nl>
2019-08-01 11:04:26 +0200

    Cleanup/clarify VomsProxyInfoServlet
    Although currently not used by the VO portal, cleanup VomsProxyInfoServlet code
    and create a commented-out section in web.xml to clarify the use of it.
    Added a comment at the top of VomsProxyInfoServlet to stress that it's not being
    used.
    Code fixes:
    - put proxy filename in a variable instead of putting together twice
    - catch read errors when reading in the proxyfile even though it's unlikely to
      fail there since VPVomsProxyInfo.exec() already succesfully read the file.
      This resolves one TODO.
    - set the "username" and "userinfo" attributes (we have the former, the latter
      is set to N/A) which are used in vomsinfo.jsp

--- vo-portal/src/main/java/eu/rcauth/voportal/servlet/VomsProxyInfoServlet.java
+++ vo-portal/src/main/java/eu/rcauth/voportal/servlet/VomsProxyInfoServlet.java
@@ -12,6 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import eu.rcauth.voportal.voms.VPVomsProxyInfo;
 
+/**
+ * NOTE: currently this class is not being used by the VO-portal
+ * It takes a username parameter to get a proxy file /tmp/username.proxy
+ * and the content is printed via the vomsinfo.jsp.
+ *
+ * The current VO-portal uses instead a MasterPortal flow to obtain the proxy
+ * which is then printed using the VPOA2ReadyServlet instead.
+ */
 public class VomsProxyInfoServlet extends HttpServlet {
 
     public static final String VOMS_INFO_PAGE = "/pages/vomsinfo.jsp";
@@ -28,9 +36,10 @@ public class VomsProxyInfoServlet extends HttpServlet {
         if (username == null || username.isEmpty())
             throw new ServletException("No username specified!");
 
+        String proxyFile = PROXY_DIR + "/" + username + ".proxy";
         String vomsinfo = null;
         try {
-            vomsinfo = VPVomsProxyInfo.exec(PROXY_DIR + "/" + username + ".proxy");
+            vomsinfo = VPVomsProxyInfo.exec(proxyFile);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -38,16 +47,27 @@ public class VomsProxyInfoServlet extends HttpServlet {
         }
 
         //read proxy certificate from expected file location
-        FileInputStream fis = new FileInputStream(PROXY_DIR + "/" + username + ".proxy");
-        BufferedInputStream bis = new BufferedInputStream(fis);
+        String proxyString;
+        try {
+            FileInputStream fis = new FileInputStream(proxyFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
 
-        byte[] proxy = new byte[bis.available()];
-        // TODO note that we ignore the return values here
-        bis.read(proxy);
-        fis.close();
+            int len = bis.available();
+            byte[] proxy = new byte[len];
+            // Note: we ignore the return values here
+            int lenread=bis.read(proxy);
+            fis.close();
+            if (lenread != len) {
+                throw new IOException("Expected to read "+len+" bytes, but read() returned "+lenread);
+            }
 
-        String proxyString = new String(proxy);
+            proxyString = new String(proxy);
+        } catch (Exception e) {
+            throw new ServletException("Unable to read proxy file\n", e);
+        }
 
+        request.setAttribute("username", username);
+        request.setAttribute("userinfo", "N/A");
         request.setAttribute("vomsinfo", vomsinfo);
         request.setAttribute("proxy", proxyString);
 
